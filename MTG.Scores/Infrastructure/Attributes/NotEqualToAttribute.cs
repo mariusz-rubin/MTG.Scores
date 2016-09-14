3abb5d4cfcf2febb9cedfb9506da5af37e589ef4
wpelczar <wpelczar1@gmail.com>
2016-09-14 23:18:20 +0200

    Match properties validation

---
+++ MTG.Scores/Infrastructure/Attributes/NotEqualToAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MTG.Scores.Infrastructure.Attributes
+{
+  [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+  public sealed class NotEqualToAttribute : ValidationAttribute
+  {
+    private const string DefaultErrorMessage = "{0} cannot be the same as {1}.";
+
+    public string OtherProperty { get; private set; }
+
+    public NotEqualToAttribute(string otherProperty)
+      : base(DefaultErrorMessage)
+    {
+      if (string.IsNullOrEmpty(otherProperty))
+      {
+        throw new ArgumentNullException("otherProperty");
+      }
+
+      OtherProperty = otherProperty;
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+      return string.Format(ErrorMessageString, name, OtherProperty);
+    }
+
+    protected override ValidationResult IsValid(object value,
+                          ValidationContext validationContext)
+    {
+      if (value != null)
+      {
+        var otherProperty = validationContext.ObjectInstance.GetType()
+                           .GetProperty(OtherProperty);
+
+        var otherPropertyValue = otherProperty
+                      .GetValue(validationContext.ObjectInstance, null);
+
+        if (value.Equals(otherPropertyValue))
+        {
+          return new ValidationResult(
+            FormatErrorMessage(validationContext.DisplayName));
+        }
+      }
+
+      return ValidationResult.Success;
+    }
+  }
+}
--- MTG.Scores/Models/Match.cs
+++ MTG.Scores/Models/Match.cs
@@ -1,15 +1,19 @@
-﻿using System.ComponentModel;
+﻿using MTG.Scores.Infrastructure.Attributes;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
 
 namespace MTG.Scores.Models
 {
-  public class Match
+  public class Match : IValidatableObject
   {
     public int ID { get; set; }
 
     public int Player1ID { get; set; }
 
+    [NotEqualTo("Player1ID", ErrorMessage = "Gracz 1 nie może być taki sam jak gracz 2")]
     public int Player2ID { get; set; }
 
     [DisplayName("Wynik gracza 1")]
@@ -25,5 +29,13 @@ namespace MTG.Scores.Models
 
     [InverseProperty("AwayMatches")]
     public virtual Player Player2 { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+      if((Player1Score != 2 && Player2Score != 2) || (Player1Score == Player2Score))
+      {
+        yield return new ValidationResult("Niepoprawny wynik");
+      } 
+    }
   }
 }

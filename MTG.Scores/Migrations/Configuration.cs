37d3ab6d1cbf52aed9a1847e623d07738cd1dff2
wpelczar <wpelczar1@gmail.com>
2016-09-24 14:33:38 +0200

    Move seed method to Configuration class

--- MTG.Scores/Migrations/Configuration.cs
+++ MTG.Scores/Migrations/Configuration.cs
@@ -1,32 +1,52 @@
 namespace MTG.Scores.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+  using Models;
+  using System.Collections.Generic;
+  using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<MTG.Scores.DataAccess.MtgContext>
+  internal sealed class Configuration : DbMigrationsConfiguration<DataAccess.MtgContext>
+  {
+    public Configuration()
     {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = false;
-            ContextKey = "MTG.Scores.DataAccess.MtgContext";
-        }
-
-        protected override void Seed(MTG.Scores.DataAccess.MtgContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
+      AutomaticMigrationsEnabled = false;
+      ContextKey = "MTG.Scores.DataAccess.MtgContext";
     }
+
+    protected override void Seed(DataAccess.MtgContext context)
+    {
+      int numberOfPlayers = 10;
+
+      var players = new List<Player>();
+
+      for (int i = 0; i < numberOfPlayers; i++)
+      {
+        players.Add(new Player
+        {
+          Name = "Player " + i.ToString(),
+        });
+      }
+
+      context.Players.AddOrUpdate(
+        p => p.Name,
+        players.ToArray());
+
+      context.SaveChanges();
+
+      var matches = new List<Match>();
+
+      matches.Add(new Match { Player1Score = 2, Player2Score = 0, Player1ID = players[0].ID, Player2ID = players[1].ID });
+      matches.Add(new Match { Player1Score = 2, Player2Score = 1, Player1ID = players[1].ID, Player2ID = players[2].ID });
+      matches.Add(new Match { Player1Score = 2, Player2Score = 0, Player1ID = players[2].ID, Player2ID = players[3].ID });
+      matches.Add(new Match { Player1Score = 0, Player2Score = 2, Player1ID = players[3].ID, Player2ID = players[4].ID });
+      matches.Add(new Match { Player1Score = 1, Player2Score = 2, Player1ID = players[4].ID, Player2ID = players[5].ID });
+
+      context.Matches.AddOrUpdate(
+        m => new { m.Player1ID, m.Player2ID },
+        matches.ToArray());
+
+      context.SaveChanges();
+
+      base.Seed(context);
+    }
+  }
 }
